88f81094951865d4aa81fb91c19c625f760ec7e7
Iwakunileola <iwakunileola13@gmail.com>
2022-10-15 09:48:41 -0700

    c programming

--- 0x00-hello_world/1-main.c
+++ 0x00-hello_world/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *Return: Always 0 (Success)
@@ -8,8 +9,6 @@
 int main(void)
 
 {
-
-	printf("wonderful Lord\n");
+	printf("wonderful Lord");
 	return (0);
-
 }
