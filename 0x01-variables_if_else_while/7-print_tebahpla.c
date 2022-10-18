2bfa03658b51ace1a6babccda012d377f75ff5dc
Iwakunileola <iwakunileola13@gmail.com>
2022-10-18 10:03:28 -0700

    variables if else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char y;
+
+	for (y = 'z'; y >= 'a' ; y--)
+
+	{
+		putchar(y);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
