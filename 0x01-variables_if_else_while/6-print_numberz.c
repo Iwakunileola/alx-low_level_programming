72f16abcf93df6096c58e18a6badf82aa85a48c4
Iwakunileola <iwakunileola13@gmail.com>
2022-10-18 14:48:03 -0700

    variables if else while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'd'; ch <= 'm'; ch++)
+
+	{
+		putchar(ch % 10 + '0');
+	}
+
+		putchar('\n');
+
+	return (0);
+}
