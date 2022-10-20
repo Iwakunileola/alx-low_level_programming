3e847dd9c4b01fd1d47551570784a4e7bf9cc266
Iwakunileola <iwakunileola13@gmail.com>
2022-10-19 21:58:26 -0700

    fuctions in C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ * Return: Always nothing
+ */
+
+void print_alphabet(void)
+{
+	int _putchar(char);
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,3 @@
+int _puthar(char c);
+void print_alphabet(void);
 
