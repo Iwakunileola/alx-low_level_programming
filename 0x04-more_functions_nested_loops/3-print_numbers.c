09853e2abef07c18807a8d4c3b1b87f6121e9849
Iwakunileola <iwakunileola13@gmail.com>
2022-10-20 18:47:47 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers frmo 0 -9
+ * @x: integer variable to print 0-9
+ * Return: void
+ */
+int print_numbers(void)
+{
+	int y:
+
+	for (y = 0; y <= 9; y++)
+	{
+		_putchar(y);
+	}
+	_putchar('\n');	
+}
