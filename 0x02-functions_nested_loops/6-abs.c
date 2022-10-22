a057d4461ec992746c31810674adbfa48456cc29
Iwakunileola <iwakunileola13@gmail.com>
2022-10-22 11:27:13 -0700

    functions and neted loop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute value of integer
+ *@y: integer to be printed
+ *Return: integer
+ */
+int _abs(int y)
+{
+	if (y < 0)
+	{
+		y = y * -1;
+	}
+	return (y);
+
+}
