f2787d772e9fdc2a3d7eb3c01e2a10d92d54047e
Iwakunileola <iwakunileola13@gmail.com>
2022-10-22 08:28:24 -0700

    functions snd nested loop in C

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isalpha - Checks for lower and upper characters
+ *@c: Integer tested
+ *Return: 1 if lower character, and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'B'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
