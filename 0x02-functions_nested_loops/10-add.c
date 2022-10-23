67558c0a7f882c491b1968428388a34d87726851
Iwakunileola <iwakunileola13@gmail.com>
2022-10-22 17:10:35 -0700

    functions and nested loops on C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - Adds two numbers
+ *@y: First integer to be added
+ *@x: Second integer to be added
+ *Return: y + x
+ */
+int add(int y, int x)
+{
+	int r;
+
+	r = y + x;
+	return (r);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
