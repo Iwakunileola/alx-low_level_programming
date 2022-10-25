c930a61805de3f79e9b4a55af018ad0fd34cfb8b
Iwakunileola <iwakunileola13@gmail.com>
2022-10-24 18:30:58 -0700

    pointers in C

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * void swap_int - update value it points to 98
+ * @a: Integer pointer parameter
+ * @b: Integer pointer parameter
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
