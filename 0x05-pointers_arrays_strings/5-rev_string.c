ef106fcc0ff97d25b5515665470a39f2f296c41e
Iwakunileola <iwakunileola13@gmail.com>
2022-10-25 17:32:31 -0700

    pointers arrays and strings in C

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *rev_string - changes strings to out put in reverse
+ *@s: Char pointer paramete/array
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	int n;
+	int p;
+	
+	s = char ch[i++];
+	for (i = 0; (s[i] != '\0'); i++)
+	{
+		s = s[i];
+		n = i;
+	}
+	for (p = n; p >= 0; p--)
+	{
+		s = s[n];
+
+	}
+}
+
