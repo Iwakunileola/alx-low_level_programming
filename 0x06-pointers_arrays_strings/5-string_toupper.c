8770e5fff1e22d38338fc3aaf6127d4fb8ea5238
Iwakunileola <iwakunileola13@gmail.com>
2022-10-27 22:52:01 -0700

    pointers arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,15 @@
 
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i]; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] >= 'a' &&  s[i] >=  'z')
 		{
 			s[i] -= 32;
 		}
+		i++;
 	}
 	return (s);
 }
