83acf038dec1786f63ebea403e9fc9f014d2d4b6
Iwakunileola <iwakunileola13@gmail.com>
2022-10-27 22:47:43 -0700

    pointers strings and arrays in C

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
 		if (s[i] >= 'a' &&  s[i] >=  'z')
 		{
 			s[i] -= 32;
 		}
-		s[i] = '\0';
 	}
 	return (s);
 }
