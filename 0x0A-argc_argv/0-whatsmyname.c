244bb73c15b8bdb9321b886cc51bcabf1e19f5ff
Iwakunileola <iwakunileola13@gmail.com>
2022-11-04 12:38:37 -0700

    Working command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Program that prints its name
+ * @argc: count parametrs in argv
+ * @argv: pointers to string
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
