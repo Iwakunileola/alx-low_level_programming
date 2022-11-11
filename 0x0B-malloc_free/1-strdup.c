2a3736b463b7efe48c3592491d502fc3dd1fe059
Iwakunileola <iwakunileola13@gmail.com>
2022-11-11 11:27:13 -0800

    Working with malloc and free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,24 +9,34 @@
 
 char *_strdup(char *str)
 {
-	int i, j;
-	char *ptr;
+	int i, j, len;
+	char *ptr, *st;
 
-	for (j = 0; str[j] != '\0'; j++)
+	st = "";
 
-	ptr = malloc(j * (sizeof(char)));
-	if (str == NULL)
+	len = 0;
+	for (j = 0; str[j]; j++)
+	{
+		len++;
+	}
+
+
+	if (str == NULL || str == st)
 	{
 		return (NULL);
 	}
+	ptr = malloc((len + 1) * sizeof(char));
+
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < j; i++)
+
+	for (i = 0; str[i]; i++)
 	{
 		ptr[i] = str[i];
 	}
-	ptr[i] = '\0';
+
+	ptr[len] = '\0';
 	return (ptr);
 }
